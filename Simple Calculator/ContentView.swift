39372de8bcf5f6747ad64f0110bdc27f54f96259
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-11-17 11:14:10 -0500

    Copied the Square Counter app, to have essential code

--- Simple Calculator/ContentView.swift
+++ Simple Calculator/ContentView.swift
@@ -1,19 +1,44 @@
 //
 //  ContentView.swift
-//  Simple Calculator
+//  Square Counter App
 //
-//  Created by Nikita Zhdanov on 2023-11-17.
+//  Created by Nikita Zhdanov on 2023-11-16.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var base: Int = 1
+    var squared: Int {
+        return base * base
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            HStack(alignment: .top) {
+                
+                Text("(\(base))")
+                    .font(.system(size: 96))
+                
+                Text("2")
+                    .font(.system(size: 44))
+                
+                Text("=")
+                    .font(.system(size: 96))
+                
+                Text("\(squared)")
+                    .font(.system(size: 96))
+            }
+            
+            Stepper(value: $base, label: {
+                Text("Base")
+            })
+            
+            Spacer()
         }
         .padding()
     }
