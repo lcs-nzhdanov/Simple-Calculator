4297da7b8b875abc6d1fb91623cc0d888de87634
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2023-11-17 11:13:09 -0500

    Initial Commit

---
+++ Simple Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple Calculator
+//
+//  Created by Nikita Zhdanov on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
